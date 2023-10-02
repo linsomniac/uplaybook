ab5304e1f76a44419b77bf272f6278326bd4975a
Sean Reifschneider <sean@realgo.com>
2023-10-02 05:25:26 -0600

    Adding more magic, cleaning up mkdir

--- uplaybook2/fs.py
+++ uplaybook2/fs.py
@@ -1,13 +1,44 @@
 #!/usr/bin/env python3
 
-from .internals import Return
+from .internals import Return, BaseTask, make_task
 from typing import Union, Optional
 import symbolicmode
 import os
 import stat
 
 
-class mkdirClass:
+def _mode_from_arg(mode: Optional[Union[str, int]] = None, initial_mode: Optional[int] = None, is_directory: bool=False) -> Optional[int]:
+    if type(mode) is int or mode is None:
+        return mode
+
+    assert type(mode) is str
+
+    mode_is_sym_str = type(mode) is str and not set(mode).issubset("01234567")
+    if mode_is_sym_str:
+        extra_args = {}
+        if initial_mode:
+            extra_args['initial_mode'] = initial_mode
+
+        return symbolicmode.symbolic_to_numeric_permissions(mode, is_directory=is_directory, **extra_args)
+
+    return int(mode, 8)
+
+def _chmod(path: str, mode: Optional[Union[str, int]] = None, is_directory: bool=False) -> Return:
+    if mode is None:
+        return Return(changed=False)
+
+    current_mode = stat.S_IMODE(os.stat(path).st_mode)
+    mode = _mode_from_arg(mode, initial_mode=current_mode, is_directory=is_directory)
+    if current_mode != mode:
+        assert type(mode) is int
+        os.chmod(path, mode)
+        return Return(changed=True, extra_message="Changed permissions")
+
+    return Return(changed=False)
+
+
+@make_task
+class mkdirClass(BaseTask):
     name = "mkdir"
 
     def __init__(self):
@@ -16,31 +47,14 @@ class mkdirClass:
     def __call__(self, path: str, mode: Optional[Union[str, int]] = None) -> Return:
         new_mode = mode
         if not os.path.exists(path):
-            if type(new_mode) == str:
-                new_mode = symbolicmode.symbolic_to_numeric_permissions(
-                    new_mode, is_directory=True
-                )
+            new_mode = _mode_from_arg(new_mode, is_directory=True)
 
-            if new_mode is None:
-                os.mkdir(path)
-            else:
-                os.mkdir(path, new_mode)
+            mode_arg = {}
+            if new_mode is not None:
+                mode_arg = {'mode': new_mode}
+
+            os.mkdir(path, **mode_arg)
 
             return Return(changed=True)
 
-        if not new_mode:
-            return Return(changed=False)
-
-        current_mode = stat.S_IMODE(os.stat(path).st_mode)
-        if type(new_mode) == str:
-            new_mode = symbolicmode.symbolic_to_numeric_permissions(
-                new_mode, initial_mode=current_mode, is_directory=True
-            )
-        if current_mode != new_mode:
-            os.chmod(path, new_mode)
-            return Return(changed=True, msg="Changed permissions")
-
-        return Return(changed=False)
-
-
-mkdir = mkdirClass()
+        return _chmod(path, new_mode, is_directory=True)
--- uplaybook2/internals.py
+++ uplaybook2/internals.py
@@ -5,19 +5,37 @@ import inspect
 from typing import Optional
 
 
+def make_task(cls):
+    stack = inspect.stack()
+    caller_frame = stack[1].frame
+    caller_namespace = caller_frame.f_globals
+    caller_namespace[cls.name] = cls()
+
+
+class BaseTask:
+    @property
+    def name(self):
+        return self.__class__.__name__
+
+
 class Return:
-    def __init__(self, changed: bool, msg: Optional[str] = None):
+    def __init__(self, changed: bool, extra_message: Optional[str] = None):
         self.changed = changed
+        self.extra_message = extra_message
+        self.print_status()
 
-        parent_frame = inspect.currentframe().f_back
-        # parent_locals = parent_frame.f_locals
-        args, _, _, values = inspect.getargvalues(parent_frame)
-        parent_function_name = values["self"].name
-        call_args = ", ".join([f"{arg}={values[arg]}" for arg in args if arg != "self"])
-        add_msg = f" ({msg})" if msg else ""
-        prefix = "=>" if changed else "=="
-        print(f"{prefix} {parent_function_name}({call_args}){add_msg}")
+    def print_status(self):
+        for frame_info in inspect.stack():
+            frame_self = frame_info.frame.f_locals.get('self', None)
+            if frame_self and isinstance(frame_self, BaseTask):
+                args, _, _, values = inspect.getargvalues(frame_info.frame)
+                call_args = ", ".join([f"{arg}={values[arg]}" for arg in args if arg != "self"])
 
+                parent_function_name = values["self"].name
+                add_msg = f" ({self.extra_message})" if self.extra_message else ""
+                prefix = "=>" if self.changed else "=="
+                print(f"{prefix} {parent_function_name}({call_args}){add_msg}")
+                break
 
 
 class UpContext:
@@ -25,6 +43,7 @@ class UpContext:
         self.context = {}
 
 up_context = UpContext()
+
 
 def cli():
     with open(sys.argv[1], "r") as fp:
