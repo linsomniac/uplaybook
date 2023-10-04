ef8d869ed0439fcef3afd5529a33f4b7d3abf541
Sean Reifschneider <sean@realgo.com>
2023-10-03 21:10:29 -0600

    Changing tasks to functions

--- uplaybook2/fs.py
+++ uplaybook2/fs.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
-from .internals import Return, BaseTask, make_task
+from .internals import Return
 from typing import Union, Optional
 import symbolicmode
 import os
 import stat
 
 
-def _mode_from_arg(mode: Optional[Union[str, int]] = None, initial_mode: Optional[int] = None, is_directory: bool=False) -> Optional[int]:
+def _mode_from_arg(
+    mode: Optional[Union[str, int]] = None,
+    initial_mode: Optional[int] = None,
+    is_directory: bool = False,
+) -> Optional[int]:
     if type(mode) is int or mode is None:
         return mode
 
@@ -17,13 +21,18 @@ def _mode_from_arg(mode: Optional[Union[str, int]] = None, initial_mode: Optiona
     if mode_is_sym_str:
         extra_args = {}
         if initial_mode:
-            extra_args['initial_mode'] = initial_mode
+            extra_args["initial_mode"] = initial_mode
 
-        return symbolicmode.symbolic_to_numeric_permissions(mode, is_directory=is_directory, **extra_args)
+        return symbolicmode.symbolic_to_numeric_permissions(
+            mode, is_directory=is_directory, **extra_args
+        )
 
     return int(mode, 8)
 
-def _chmod(path: str, mode: Optional[Union[str, int]] = None, is_directory: bool=False) -> Return:
+
+def _chmod(
+    path: str, mode: Optional[Union[str, int]] = None, is_directory: bool = False
+) -> Return:
     if mode is None:
         return Return(changed=False)
 
@@ -37,59 +46,38 @@ def _chmod(path: str, mode: Optional[Union[str, int]] = None, is_directory: bool
     return Return(changed=False)
 
 
-@make_task
-class mkfileClass(BaseTask):
-    name = "mkfile"
+def mkfile(path: str, mode: Optional[Union[str, int]] = None) -> Return:
+    new_mode = mode
+    if not os.path.exists(path):
+        new_mode = _mode_from_arg(new_mode)
+        mode_arg = {} if new_mode is None else {"mode": new_mode}
+        fd = os.open(path, os.O_CREAT, **mode_arg)
+        os.close(fd)
 
-    def __init__(self):
-        pass
+        return Return(changed=True)
 
-    def __call__(self, path: str, mode: Optional[Union[str, int]] = None) -> Return:
-        new_mode = mode
-        if not os.path.exists(path):
-            new_mode = _mode_from_arg(new_mode)
-            mode_arg = {} if new_mode is None else {'mode': new_mode}
-            fd = os.open(path, os.O_CREAT, **mode_arg)
-            os.close(fd)
-
-            return Return(changed=True)
-
-        return _chmod(path, new_mode)
+    return _chmod(path, new_mode)
 
 
-@make_task
-class mkdirClass(BaseTask):
-    name = "mkdir"
+def mkdir(path: str, mode: Optional[Union[str, int]] = None) -> Return:
+    new_mode = mode
+    if not os.path.exists(path):
+        new_mode = _mode_from_arg(new_mode, is_directory=True)
+        mode_arg = {} if new_mode is None else {"mode": new_mode}
+        os.mkdir(path, **mode_arg)
 
-    def __init__(self):
-        pass
+        return Return(changed=True)
 
-    def __call__(self, path: str, mode: Optional[Union[str, int]] = None) -> Return:
-        new_mode = mode
-        if not os.path.exists(path):
-            new_mode = _mode_from_arg(new_mode, is_directory=True)
-            mode_arg = {} if new_mode is None else {'mode': new_mode}
-            os.mkdir(path, **mode_arg)
-
-            return Return(changed=True)
-
-        return _chmod(path, new_mode, is_directory=True)
+    return _chmod(path, new_mode, is_directory=True)
 
 
-@make_task
-class makedirsClass(BaseTask):
-    name = "makedirs"
+def makedirs(path: str, mode: Optional[Union[str, int]] = None) -> Return:
+    new_mode = mode
+    if not os.path.exists(path):
+        new_mode = _mode_from_arg(new_mode, is_directory=True)
+        mode_arg = {} if new_mode is None else {"mode": new_mode}
+        os.makedirs(path, **mode_arg)
 
-    def __init__(self):
-        pass
+        return Return(changed=True)
 
-    def __call__(self, path: str, mode: Optional[Union[str, int]] = None) -> Return:
-        new_mode = mode
-        if not os.path.exists(path):
-            new_mode = _mode_from_arg(new_mode, is_directory=True)
-            mode_arg = {} if new_mode is None else {'mode': new_mode}
-            os.makedirs(path, **mode_arg)
-
-            return Return(changed=True)
-
-        return _chmod(path, new_mode, is_directory=True)
+    return _chmod(path, new_mode, is_directory=True)
--- uplaybook2/internals.py
+++ uplaybook2/internals.py
@@ -5,19 +5,6 @@ import inspect
 from typing import Optional
 
 
-def make_task(cls):
-    stack = inspect.stack()
-    caller_frame = stack[1].frame
-    caller_namespace = caller_frame.f_globals
-    caller_namespace[cls.name] = cls()
-
-
-class BaseTask:
-    @property
-    def name(self):
-        return self.__class__.__name__
-
-
 class Return:
     def __init__(self, changed: bool, extra_message: Optional[str] = None):
         self.changed = changed
@@ -25,22 +12,19 @@ class Return:
         self.print_status()
 
     def print_status(self):
-        for frame_info in inspect.stack():
-            frame_self = frame_info.frame.f_locals.get('self', None)
-            if frame_self and isinstance(frame_self, BaseTask):
-                args, _, _, values = inspect.getargvalues(frame_info.frame)
-                call_args = ", ".join([f"{arg}={values[arg]}" for arg in args if arg != "self"])
-
-                parent_function_name = values["self"].name
-                add_msg = f" ({self.extra_message})" if self.extra_message else ""
-                prefix = "=>" if self.changed else "=#"
-                print(f"{prefix} {parent_function_name}({call_args}){add_msg}")
-                break
+        parent_frame_info = inspect.stack()[2]
+        parent_function_name = parent_frame_info.function
+        args, _, _, values = inspect.getargvalues(parent_frame_info.frame)
+        call_args = ", ".join([f"{arg}={values[arg]}" for arg in args if arg != "self"])
+        add_msg = f" ({self.extra_message})" if self.extra_message else ""
+        prefix = "=>" if self.changed else "=#"
+        print(f"{prefix} {parent_function_name}({call_args}){add_msg}")
 
 
 class UpContext:
     def __init__(self):
         self.context = {}
+
 
 up_context = UpContext()
 
