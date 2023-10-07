b84d49b9e6752b6784ac434d5f8abea40d450499
Sean Reifschneider <sean@realgo.com>
2023-10-07 06:07:29 -0600

    Modifying how we load the playbook, globals no longer needed

--- uplaybook2/internals.py
+++ uplaybook2/internals.py
@@ -2,8 +2,8 @@
 
 import sys
 import inspect
-from typing import Optional, Union
-from typing import Callable, Any
+from typing import Optional, Union, List, Callable, Any
+from types import ModuleType
 from functools import wraps
 import jinja2
 import os
@@ -347,20 +347,75 @@ def extract_docstring_from_file(filename: str) -> Union[str, None]:
     return None
 
 
+def import_script_as_module(module_name: str, paths_to_try: List[str]) -> ModuleType:
+    """
+    Imports a Python script as a module, whether it ends in ".py" or not.
+    Given the name of a module to import, and a list of absolute or relative path names
+    (including the filename), import the module.  The module is set up so that it can
+    later be imported, but a reference to the module is also returned.
+
+    Args:
+        module_name (str): The name of the module to import.
+                This doesn't have to match the filename.
+        paths_to_try (List[str]): A list of file paths to look for the file to load.
+                This can be absolute or relative paths to the file, the first file that
+                exists is used.
+
+    Returns:
+        Module: A reference to the imported module.
+
+    Raises:
+        FileNotFoundError: If the module file is not found in any of the specified directory paths.
+        ImportError: If there are issues importing the module, such as invalid Python syntax in the module file.
+
+    Example:
+        my_module = import_script_as_module("my_module", ["my_module", "../my_module"])
+
+        # Now you can either directly use "my_module"
+        my_module.function()
+
+        # Or you can later import it:
+        import my_module
+    """
+    from pathlib import Path
+    import os
+
+    for try_filename in paths_to_try:
+        if os.path.exists(try_filename):
+            module_filename = Path(try_filename).resolve()
+            break
+    else:
+        raise FileNotFoundError(f"Unable to find '{module_name}' module to import")
+
+    from importlib.util import spec_from_loader, module_from_spec
+    from importlib.machinery import SourceFileLoader
+    import sys
+
+    spec = spec_from_loader(
+        module_name, SourceFileLoader(module_name, str(module_filename))
+    )
+    if spec is None:
+        raise ImportError("Unable to spec_from_loader() the module, no error returned.")
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+    sys.modules["up"] = module
+
+    return module
+
+
 def cli() -> None:
     """
     The main entry point for the CLI.
     """
-    with open(sys.argv[1], "r") as fp:
-        playbook = fp.read()
-        try:
-            exec(playbook, globals(), locals())
-        except Exception:
-            print(traceback.format_exc())
+    try:
+        pb_name = sys.argv[1]
+        pb = import_script_as_module(pb_name, [pb_name, f"./{pb_name}"])
+    except Exception:
+        print(traceback.format_exc())
 
-        up_context.flush_handlers()
+    up_context.flush_handlers()
 
-        print()
-        print(
-            f"*** RECAP:  total={up_context.total_count} changed={up_context.changed_count} failure={up_context.failure_count}"
-        )
+    print()
+    print(
+        f"*** RECAP:  total={up_context.total_count} changed={up_context.changed_count} failure={up_context.failure_count}"
+    )
