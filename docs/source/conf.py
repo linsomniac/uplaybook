7b12f9075fc7f87e3dbf579c7df048b4e87675e4
Sean Reifschneider <sean@realgo.com>
2023-10-25 07:42:31 -0600

    Trying to add fs docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,1 +1,19 @@
 html_theme = "alabaster"  # or another theme of your choice
+extensions = [
+    "m2r2",
+    "sphinx.ext.autodoc",
+]
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
+
+def skip(app, what, name, obj, would_skip, options):
+    if "#taskdoc" not in obj.__doc__:
+        return True
+    return would_skip
+
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
---
+++ docs/source/fs.md
@@ -0,0 +1,7 @@
+# Filesystem Tasks
+
+<!-- Your other content -->
+
+```eval_rst
+.. automodule:: uplaybook.fs
+    :members:
